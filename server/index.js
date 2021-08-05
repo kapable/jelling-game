3c1a832819536c794312ef135d1fda130fe08730
kapable <skjoldenstore@gmail.com>
2021-08-05 23:15:20 +0900

    add user auth and logout

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const config = require('./config/dev')
 const { User } = require('./models/User')
+const { auth } = require('./middleware/auth')
 
 const port = 5000
 
@@ -20,7 +21,15 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.post('/register', (req, res) => {
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/api/hello', (req, res) => {
+    res.send("hello everyone!")
+})
+
+app.post('/api/users/register', (req, res) => {
     // 회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
     
     const user = new User(req.body)
@@ -33,7 +42,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일이 데이터 베이스에 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user) {
@@ -59,8 +68,25 @@ app.post('/login', (req, res) => {
     })
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 의미는, Authentication === True
+    res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token:"" }, (err, user) => {
+    if(err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+    })
 })
 
 app.listen(port, () => {
