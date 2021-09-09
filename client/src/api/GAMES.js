e37cd82f4828b1aa72f84f72f9fe6f64087b08e7
kapable <skjoldenstore@gmail.com>
2021-09-09 17:47:42 +0900

    apply react snap and s3 images bucket

--- client/src/api/GAMES.js
+++ client/src/api/GAMES.js
@@ -4,42 +4,10 @@ const GAMES = [
         title:"Palette Game",
         sourceURL:"http://jelling.s3-website.ap-northeast-2.amazonaws.com/Games/palette/index.html",
         mainUrl:"palette",
-        thumbImage:"https://images.ktestone.com/main-thumbnail/union-thumb.png",
-        introImage:"https://images.ktestone.com/introImages/union-intro.png",
+        thumbImage:"https://jellingimages.s3.ap-northeast-2.amazonaws.com/thumbImages/palette-thumb.png",
+        introImage:"https://jellingimages.s3.ap-northeast-2.amazonaws.com/introImages/palette-intro.png",
         lang:"Eng"
     },
-    {
-        title:"Popit Game",
-        sourceURL:"https://joungbins.github.io/popit/",
-        mainUrl:"popit",
-        thumbImage:"https://images.ktestone.com/main-thumbnail/union-thumb.png",
-        introImage:"https://images.ktestone.com/introImages/union-intro.png",
-        lang:"Kor"
-    },
-    {
-        title:"Line Game",
-        sourceURL:"https://joungbins.github.io/line/",
-        mainUrl:"line",
-        thumbImage:"https://images.ktestone.com/main-thumbnail/union-thumb.png",
-        introImage:"https://images.ktestone.com/introImages/union-intro.png",
-        lang:"Kor"
-    },
-    {
-        title:"Boxbuliding Game",
-        sourceURL:"https://joungbins.github.io/boxbuliding/",
-        mainUrl:"boxbuliding",
-        thumbImage:"https://images.ktestone.com/main-thumbnail/union-thumb.png",
-        introImage:"https://images.ktestone.com/introImages/union-intro.png",
-        lang:"Kor"
-    },
-    {
-        title:"Colormatch Game",
-        sourceURL:"https://joungbins.github.io/colormatch/",
-        mainUrl:"colormatch",
-        thumbImage:"https://images.ktestone.com/main-thumbnail/union-thumb.png",
-        introImage:"https://images.ktestone.com/introImages/union-intro.png",
-        lang:"Kor"
-    }
 ]
 
 export default GAMES;
