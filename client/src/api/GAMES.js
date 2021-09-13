b025c42f4e5d70382c332161ec37e2b493c6b81f
kapable <skjoldenstore@gmail.com>
2021-09-13 17:21:54 +0900

    add colormatch game

--- client/src/api/GAMES.js
+++ client/src/api/GAMES.js
@@ -10,8 +10,19 @@ const GAMES = [
         lang:"Eng",
         desc:`- It is a game where paint is dropped and synthesized by touch.\n
         - Finally, making a palette and increasing the score.`,
-        descKor:` - 터치 방식으로 물감을 드롭하여 합성하는 게임 입니다.\n
+        descKor:`- 터치 방식으로 물감을 드롭하여 합성하는 게임 입니다.\n
                 - 최종적으로 팔레트를 만들고 점수를 높히는 방식의 게임`,
+    },
+    {
+        title:"Color Match",
+        titleKor:"컬러 매치",
+        sourceURL:"https://jellinggame.com/Games/colormatch/index.html",
+        mainUrl:"colormatch",
+        thumbImage:"https://images.jellinggame.com/thumbImages/colormatch-thumb.png",
+        introImage:"https://images.jellinggame.com/introImages/colormatch-intro.png",
+        lang:"Eng",
+        desc:`- A game that changes the color of the bar at the bottom according to the falling circle as a click-based game`,
+        descKor:`- 클릭 방식의 게임으로 내려오는 원에 맞춰 하단에 바의 색상을 변경하는 게임`,
     },
 ]
 
