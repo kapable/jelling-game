3c063f211b9ed76592a550ad958f473dad23c823
kapable <skjoldenstore@gmail.com>
2021-09-23 15:50:57 +0900

    fix long touch context menu

--- client/src/components/Defaults/GamePage.js
+++ client/src/components/Defaults/GamePage.js
@@ -4,12 +4,32 @@ import MetaTagRenderer from './MetaTagRenderer';
 
 function GamePage(props) {
     useEffect(() => {
+        function absorbEvent_(event) {
+            var e = event || window.event;
+            e.preventDefault && e.preventDefault();
+            e.stopPropagation && e.stopPropagation();
+            e.cancelBubble = true;
+            e.returnValue = false;
+            return false;
+        }
+    
+        function preventLongPressMenu(nodes) {
+            for(var i=0; i<nodes.length; i++){
+            nodes[i].ontouchstart = absorbEvent_;
+            nodes[i].ontouchmove = absorbEvent_;
+            nodes[i].ontouchend = absorbEvent_;
+            nodes[i].ontouchcancel = absorbEvent_;
+            }
+        }
+        preventLongPressMenu(document.querySelectorAll('div'));
         window.oncontextmenu = function(event) {
             event.preventDefault();
             event.stopPropagation();
             return false;
         };
     }, [])
+
+    
 
     return (
         <div className="game-div">
